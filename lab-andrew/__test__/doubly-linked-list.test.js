94358cb1b27793ce4c8520023890c3eb8e29e6e3
Andrew Bloom <blooma116@gmail.com>
2017-12-09 14:40:38 -0800

    progress on doubly-linked-list delete method

--- lab-andrew/__test__/doubly-linked-list.test.js
+++ lab-andrew/__test__/doubly-linked-list.test.js
@@ -5,6 +5,7 @@ const DoublyLinkedList = require('../model/doubly-linked-list');
 describe('doubly-linked-list.js',() => {
   test('A list with a single node, should have a value and no next or prev', () => {
     let result = new DoublyLinkedList(1);
+    console.log(result);
     expect(result.value).toEqual(1);
     expect(result.next).toEqual(null);
     expect(result.prev).toEqual(null);
@@ -14,6 +15,7 @@ describe('doubly-linked-list.js',() => {
     let result = new DoublyLinkedList(1);
     result.append(new DoublyLinkedList(2));
     result.append(new DoublyLinkedList(3));
+    console.log(result);
 
     expect(result.value).toEqual(1);
     expect(result.prev).toEqual(null);
@@ -26,4 +28,74 @@ describe('doubly-linked-list.js',() => {
 
     expect(result.next.next.next).toEqual(null);
   });
+
+  test('deletion should properly modify the next property and new node\'s prev should point to previous node\'s value', () => {
+    let first = new DoublyLinkedList(10);
+    let second = new DoublyLinkedList(20);
+    let third = new DoublyLinkedList(30);
+
+    first.append(second);
+    first.append(third);
+
+    first.delete(second);
+
+    expect(first.value).toEqual(10);
+    expect(first.next.value).toEqual(30);
+    expect(first.next.prev.value).toEqual(10);
+    expect(first.next.next).toEqual(null);
+
+  });
+
+  test('delete should be able to erase the first element of a DoublyLinkedList', () => {
+    let first = new DoublyLinkedList(10);
+    let second = new DoublyLinkedList(20);
+    let third = new DoublyLinkedList(30);
+
+    first.append(second);
+    first.append(third);
+
+    first.delete(first);
+
+    expect(first.value).toEqual(20);
+    expect(first.prev).toEqual(null);
+    expect(first.next.value).toEqual(30);
+    expect(first.next.prev.value).toEqual(20);
+    expect(first.next.next).toEqual(null);
+  });
+
+  test('delete should be able to erase the last element of a DoublyLinkedList', () => {
+    let first = new DoublyLinkedList(10);
+    let second = new DoublyLinkedList(20);
+    let third = new DoublyLinkedList(30);
+
+    first.append(second);
+    first.append(third);
+
+    first.delete(third);
+
+    expect(first.value).toEqual(10);
+    expect(first.prev).toEqual(null);
+    expect(first.next.value).toEqual(20);
+    expect(first.next.prev.value).toEqual(10);
+    expect(first.next.next).toEqual(null);
+  });
+
+  test('delete should be able to erase the only element of a DoublyLinkedList', () => {
+    let first = new DoublyLinkedList(10);
+    let second = new DoublyLinkedList(20);
+    let third = new DoublyLinkedList(30);
+
+    first.append(second);
+    first.append(third);
+
+    first.delete(third);
+    first.delete(second);
+    first.delete(first);
+
+    expect(first.value).toEqual(undefined);
+    expect(first.prev).toEqual(undefined);
+    expect(first.next).toEqual(undefined);
+    expect(first).toEqual({});
+  });
+
 });
--- lab-andrew/model/doubly-linked-list.js
+++ lab-andrew/model/doubly-linked-list.js
@@ -5,20 +5,54 @@ class DoublyLinkedList{
   constructor(value){
     this.value = value;
     this.next = null;
-    this.previous = null;
+    this.prev = null;
   }
 
   append(node){
     if(!(node instanceof DoublyLinkedList))
       throw new TypeError('<node> should be an instance of DoublyLinkedList');
-    
+
     if(!this.next){
       this.next = node;
-      node.previous = this;
+      node.prev = this;
     }
     else
       this.next.append(node);
 
+    return this;
+  }
+
+  delete(node){
+    if(!(node instanceof DoublyLinkedList))
+      throw new TypeError('<node> should be an instance of DoublyLinkedList');
+
+    if(node.value === this.value){
+      if (this.next){
+        this.value = this.next.value;
+        this.next = this.next.next;
+        if (this.next.next)
+          this.next.next.prev = this;
+      }
+      if (!this.next){
+        delete this.next;
+        delete this.prev;
+        delete this.value;
+      }
+      return this;
+    }
+
+    if(!this.next)
+      return this;
+    if(this.next === node){
+      if(this.next.next){
+        this.next.next.prev = this;
+        this.next = this.next.next;
+      } else {
+        this.next = null;
+      }
+    } else {
+      this.next.delete(node);
+    }
     return this;
   }
 }
