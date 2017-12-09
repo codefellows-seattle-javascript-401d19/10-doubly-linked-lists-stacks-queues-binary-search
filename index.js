20677536fe25111268299bdfc1b620a1284ff8b6
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-08 20:37:31 -0800

    adding binary-search function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
-
 let binarySearch = require('./lib/binary-search');
 
-console.log([2, 4, 7, 8, 10, 19, 40, 56, 99, 399, 501], 10);
+let sampleArray = [
+  {id: 10, name : 'Demi' },
+  {id: 20, name : 'Sir Gregor'},
+  {id: 30, name : 'The Hound'},
+];
+
+console.log(binarySearch(sampleArray, 10));
