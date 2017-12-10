a503b3c784ba849e3623da1562df8e3383a8d737
snobeard <fatmattmatt@gmail.com>
2017-12-10 15:17:56 -0800

    initial commit

---
+++ lab-matt/__test__/doubly-linked-list.test.js
@@ -0,0 +1,85 @@
+'use strict';
+
+// path to link js
+const LinkedList = require('../model/doubly-linked-list');
+
+describe('linked-list.js', () => {
+  test('testing single element in list and no next', ()=> {
+    let result = new LinkedList(5);
+    expect(result.value).toEqual(5);
+    expect(result.next).toEqual(null);
+  });
+
+  test('insertion should properly modify the next property', () => {
+    let result = new LinkedList(5);
+    result.append(new LinkedList(4));
+    result.append(new LinkedList(10));
+
+    expect(result.value).toEqual(5);
+    expect(result.next.value).toEqual(4);
+    expect(result.next.next.value).toEqual(10);
+    expect(result.next.next.next).toEqual(null);
+
+    expect(() => result.append('seven')).toThrow();    
+  });
+
+  test('remove should update the next property and erase an element', () => {
+    let list = new LinkedList(10);
+    let second = new LinkedList(20);
+    let third = new LinkedList(30);
+    
+
+    list.append(second); // new LinkedList(20)
+    list.append(third); // new LinkedList(30)
+
+    expect(list.value).toEqual(10);
+    expect(list.next.value).toEqual(20);
+    expect(list.next.next.value).toEqual(30);
+    expect(list.next.next.next).toEqual(null);
+
+    list.remove(second);
+
+    expect(list.value).toEqual(10);
+    expect(list.next.value).toEqual(30);
+    expect(list.next.next).toEqual(null);
+
+    let fourth = new LinkedList(40);
+    list.append(fourth);
+
+    expect(list.next.next.value).toEqual(40);
+    expect(list.next.next.next).toEqual(null);
+    expect(() => list.remove('seven')).toThrow();
+    
+    list.remove(fourth);
+
+    expect(list.next.value).toEqual(30);
+    expect(list.next.next).toEqual(null);
+  });
+
+  test('finding given element and returning the index', () => {
+    let list = new LinkedList(10);
+    let second = new LinkedList(20);
+    let third = new LinkedList(30);
+    let shouldBeError = new LinkedList(123);
+
+    list.append(second); // new LinkedList(20)
+    list.append(third); // new LinkedList(30)
+
+    expect(list.value).toEqual(10);
+    expect(list.next.value).toEqual(20);
+    expect(list.next.next.value).toEqual(30);
+    expect(list.next.next.next).toEqual(null);
+
+    expect(list.find(10)).toEqual(list);
+    expect(list.find(list)).toEqual(list);
+    expect(list.find(20)).toEqual(second);
+    expect(list.find(second)).toEqual(second);
+    expect(list.find(30)).toEqual(third);
+    expect(list.find(third)).toEqual(third);
+
+    expect(list.find(123)).toEqual(`no data: '123' found out of 3 nodes`);
+    expect(list.find(shouldBeError)).toEqual(`no distinct node object found out of 3 nodes`);
+  });
+
+
+});
---
+++ lab-matt/model/doubly-linked-list.js
@@ -0,0 +1,64 @@
+'use strict';
+
+class LinkedList{
+
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+  append(node) {
+    if (!(node instanceof LinkedList))
+      throw new TypeError('<node> should be an instance of LinkedList');
+    
+    // mattL - we know this is the last element if there is no next (null)
+    if (!this.next) 
+      this.next = node;
+    else
+      this.next.append(node);
+    
+    return this;
+  }
+
+  remove(node) {
+    if (!(node instanceof LinkedList))
+      throw new TypeError('<node> should be an instance of LinkedList');
+
+    if (this.next.next === null && this.next === node)
+      return this.next = null;
+
+    if (this.next === node) 
+      return this.next = this.next.next;
+    else 
+      return this.next.remove(node);
+    
+
+  }
+
+  find(node, num) {
+    let counter = num + 1 || 1;
+
+    // mattL - if searching by linkedlist value
+    if (!(node instanceof LinkedList)) {
+      if (this.value === node)
+        return this;
+      
+      if (!this.next) 
+        return `no data: '${node}' found out of ${counter} nodes`;
+  
+      return this.next.find(node, counter);
+      
+    // mattL - if searching by linkedlist object    
+    } else {
+      if (this === node)
+        return this;
+
+      if (!this.next)
+        return `no distinct node object found out of ${counter} nodes`;
+    
+      return this.next.find(node, counter);
+    }
+  }     
+}
+        
+module.exports = LinkedList;
