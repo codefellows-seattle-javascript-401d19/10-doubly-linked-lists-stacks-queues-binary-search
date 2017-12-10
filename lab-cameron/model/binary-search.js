dafe9ccabac1d1a025bc3c88b1b70c0fd94e86b6
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-10 15:33:35 -0800

    binarySearch completed with tests

--- lab-cameron/model/binary-search.js
+++ lab-cameron/model/binary-search.js
@@ -1,7 +1,22 @@
 'use strict';
 
-// const binarySearch = (sortedArray, element) => {
-//
-// };
-//
-// module.exports = binarySearch;
+const binarySearch = (sortedArray, id) => {
+  let low = 0;
+  let high = sortedArray.length - 1;
+
+  while (low <= high) {
+    let pivot = Math.floor((low + high) / 2);
+    let pivotElement = sortedArray[pivot];
+    if (pivotElement.id < id) {
+      low = pivot + 1;
+    } else if (pivotElement.id > id) {
+      high = pivot - 1;
+    } else {
+      return pivotElement;
+    }
+  }
+
+  return -1;
+};
+
+module.exports = binarySearch;
