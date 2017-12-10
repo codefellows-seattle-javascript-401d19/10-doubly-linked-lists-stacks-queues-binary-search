44ea2fe834f62ba32cfea6d34e5e63da403fed77
Andrew Bloom <blooma116@gmail.com>
2017-12-10 13:06:38 -0800

    added additional tests for stacks and queues to test more complex behavior

--- lab-andrew/__test__/queue.test.js
+++ lab-andrew/__test__/queue.test.js
@@ -14,4 +14,31 @@ describe('queue.js', () => {
     expect(queue.dequeue()).toEqual(undefined);
     expect(queue.dequeue()).toEqual(undefined);
   });
+
+  test('Queue work with multiple data types', () => {
+    const queue = new Queue('hey');
+    queue.enqueue(true);
+    queue.enqueue(undefined);
+    queue.enqueue({});
+
+    expect(queue.dequeue()).toEqual('hey');
+    expect(queue.dequeue()).toEqual(true);
+    expect(queue.dequeue()).toEqual(undefined);
+    expect(queue.dequeue()).toEqual({});
+    expect(queue.dequeue()).toEqual(undefined);
+  });
+
+  test('Queue should work with complex orders', () => {
+    const queue = new Queue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+
+    expect(queue.dequeue()).toEqual(1);
+    expect(queue.dequeue()).toEqual(2);
+
+    queue.enqueue(4);
+
+    expect(queue.dequeue()).toEqual(3);
+    expect(queue.dequeue()).toEqual(4);
+  });
 });
--- lab-andrew/__test__/stack.test.js
+++ lab-andrew/__test__/stack.test.js
@@ -14,4 +14,31 @@ describe('stack.js', () => {
     expect(stack.stackPop()).toEqual(undefined);
     expect(stack.stackPop()).toEqual(undefined);
   });
+
+  test('Stack work with multiple data types', () => {
+    const stack = new Stack('hey');
+    stack.stackPush(true);
+    stack.stackPush(undefined);
+    stack.stackPush({});
+
+    expect(stack.stackPop()).toEqual({});
+    expect(stack.stackPop()).toEqual(undefined);
+    expect(stack.stackPop()).toEqual(true);
+    expect(stack.stackPop()).toEqual('hey');
+    expect(stack.stackPop()).toEqual(undefined);
+  });
+
+  test('Stack should work with complex orders', () => {
+    const stack = new Stack(1);
+    stack.stackPush(2);
+    stack.stackPush(3);
+
+    expect(stack.stackPop()).toEqual(3);
+    expect(stack.stackPop()).toEqual(2);
+
+    stack.stackPush(4);
+
+    expect(stack.stackPop()).toEqual(4);
+    expect(stack.stackPop()).toEqual(1);
+  });
 });
