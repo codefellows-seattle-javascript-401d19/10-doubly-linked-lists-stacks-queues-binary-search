9d67af7e29714da29c6d0ba63421459cc8e12393
Robert Reed <robert.mc.reed@gmail.com>
2017-12-10 09:58:16 -0800

    insertAfter tested on tail node

--- lab-rob/__test__/doublyLinkedList.test.js
+++ lab-rob/__test__/doublyLinkedList.test.js
@@ -18,14 +18,38 @@ describe('doublyLinkedList.js', () => {
   });
 
   test('Calling append on any node in the list should add it to the end of the list.', () => {
-    firstLink = new DoublyLinkedList(1);
-    secondLink = new DoublyLinkedList(2);
-    thirdLink = new DoublyLinkedList(3);
     head.append(firstLink).append(secondLink);
     firstLink.append(thirdLink);
-    console.log(head);
 
     expect(head.next.next.next).toEqual(thirdLink);
+    expect(thirdLink.previous.previous.previous).toEqual(head);
+  });
+
+  test('Calling prepend on any node in the list should add it to the beginning of the list.', () => {
+    thirdLink.prepend(secondLink).prepend(firstLink);
+    secondLink.prepend(head);
+
+    expect(head.next.next.next.value).toEqual(3);
+    expect(thirdLink.previous.previous.previous.value).toEqual(0);
+  });
+
+  test('Calling insertAfter on a non-tail node should insert a new node immediately following the calling node.', () => {
+    head.append(thirdLink);
+    expect(head.next.value).toEqual(3);
+    expect(thirdLink.previous).toEqual(head);
+
+    head.insertAfter(secondLink);
+    expect(secondLink.next.value).toEqual(3);
+    expect(thirdLink.previous.previous).toEqual(head);
+
+    head.insertAfter(firstLink);
+    expect(head.next.value).toEqual(1);
+  });
+
+  test('Calling insertAfter on a tail node should act the same way as appending a node.', () => {
+    head.insertAfter(firstLink);
+    expect(head.next.value).toEqual(1);
+    expect(firstLink.previous.value).toEqual(0);
   });
 
   test('Calling any DoublyLinkedList method with an argument that isn\'t a proper node will throw an error.', () => {
--- lab-rob/model/doublyLinkedList.js
+++ lab-rob/model/doublyLinkedList.js
@@ -14,11 +14,40 @@ class DoublyLinkedList {
     while(currentNode.next) {
       currentNode = currentNode.next;
     }
+
     currentNode.next = node;
     node.previous = currentNode;
     return this;
   }
+  
+  prepend(node) {
+    isNode(node);
+  
+    let currentNode = this;
+    while(currentNode.previous) {
+      currentNode = currentNode.previous;
+    }
+
+    currentNode.previous = node;
+    node.next = currentNode;
+    return this;
+  }
+
+  insertAfter(node) {
+    isNode(node);
+
+    if(this.next) {
+      node.next = this.next;
+      node.next.previous = node;
+    }
+
+    this.next = node;
+    node.previous = this;
+    return this;
+  }
+
 }
+
 
 module.exports = DoublyLinkedList;
 
@@ -26,3 +55,12 @@ let isNode = node => {
   if(!(node instanceof DoublyLinkedList))
     throw new TypeError('<node> must be an instance of DoublyLinkedList');
 };
+
+// let head = new DoublyLinkedList(0);
+// let firstLink = new DoublyLinkedList(1);
+// let secondLink = new DoublyLinkedList(2);
+// let thirdLink = new DoublyLinkedList(3);
+
+// head.append(firstLink).append(secondLink);
+// firstLink.append(thirdLink);
+// console.log(thirdLink.previous.previous.previous.previous);
