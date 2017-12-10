efbea65c059fca1c19463f77c500e506482d303f
Robert Reed <robert.mc.reed@gmail.com>
2017-12-10 09:18:57 -0800

    throwing test passes

--- lab-rob/__test__/doublyLinkedList.test.js
+++ lab-rob/__test__/doublyLinkedList.test.js
@@ -7,9 +7,27 @@ describe('doublyLinkedList.js', () => {
   beforeEach(() => {
     head = new DoublyLinkedList(0);
   });
+
   test('Instantiating a new object should return a node with a value and null previous/next properties.', () => {
     expect(head.previous).toBeNull();
     expect(head.value).toEqual(0);
     expect(head.next).toBeNull();
   });
+
+  test('Calling append on any node in the list should add it to the end of the list.', () => {
+    let firstLink = new DoublyLinkedList(1);
+    let secondLink = new DoublyLinkedList(2);
+    let thirdLink = new DoublyLinkedList(3);
+    head.append(firstLink).append(secondLink);
+    firstLink.append(thirdLink);
+
+    expect(head.next.next.next).toEqual(thirdLink);
+  });
+
+  test('Calling any DoublyLinkedList method with an argument that isn\'t a proper node will throw an error.', () => {
+    expect(() => {
+      head.append({previous: null, value: 10, next: null});
+    }).toThrow();
+  });
+
 });
--- lab-rob/model/doublyLinkedList.js
+++ lab-rob/model/doublyLinkedList.js
@@ -6,6 +6,23 @@ class DoublyLinkedList {
     this.value = value;
     this.next = null;
   }
+
+  append(node) {
+    isNode(node);
+
+    let currentNode = this;
+    while(currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    node.previous = currentNode;
+    return this;
+  }
 }
 
 module.exports = DoublyLinkedList;
+
+let isNode = node => {
+  if(!(node instanceof DoublyLinkedList))
+    throw new TypeError('<node> must be an instance of DoublyLinkedList');
+};
