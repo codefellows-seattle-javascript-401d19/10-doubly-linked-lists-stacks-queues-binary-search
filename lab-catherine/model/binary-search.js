2f36770487fcd1f813b6d1897b3330113fa909ef
Catherine Looper <loopercat@gmail.com>
2017-12-10 22:31:45 -0800

    Added fourth test and error handling to ensure that the item to find is a number

--- lab-catherine/model/binary-search.js
+++ lab-catherine/model/binary-search.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const binarySearch = (sortedObjectArray,itemToFind) => {
-  if(!Array.isArray(sortedObjectArray)) throw new TypeError ('The argument must be an array');
+  if(!Array.isArray(sortedObjectArray)) throw new TypeError ('The first argument must be an array');
+  if(typeof(itemToFind) !== 'number') throw new TypeError ('The second argument must be a number ');
 
   let lowIndex = 0;
   let highIndex = sortedObjectArray.length - 1;
