c20ff7dd3374576b08c3f6f818d3c7f4b6f51c76
Robert Reed <robert.mc.reed@gmail.com>
2017-12-10 18:33:07 -0800

    all binary tests passing

--- lab-rob/__test__/binary-search-objects.test.js
+++ lab-rob/__test__/binary-search-objects.test.js
@@ -22,6 +22,20 @@ describe('binary-search-objects.js', () => {
 
   test('binarySearchObjects should return the object with the given id.', () => {
     expect(binarySearchObjects(sampleArray, 60)).toEqual({ id: 60, name: 'Stella' });
+    expect(binarySearchObjects(sampleArray, 20)).toEqual({ id: 20, name: 'Sir Gregor' });
+  });
+
+  test('binarySearchObjects should throw an error if an array is not given as the first argument.', () => {
+    expect(() => {
+      binarySearchObjects('meat', 650);
+    }).toThrow();
+  });
+  
+  test('binarySearchObjects should throw an error if an element that is checked is missing it\'s id property.', () => {
+    delete sampleArray[5].id;
+    expect(() => {
+      binarySearchObjects(sampleArray, 90);
+    }).toThrow();
   });
 
   test('binarySearchObjects should throw an error if the object with the given id is not found.', () => {
