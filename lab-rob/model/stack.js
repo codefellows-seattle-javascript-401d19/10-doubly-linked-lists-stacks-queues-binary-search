f96d72ab43a53433281da3938a18918d0d708758
Robert Reed <robert.mc.reed@gmail.com>
2017-12-10 16:43:36 -0800

    stack tests passing

--- lab-rob/model/stack.js
+++ lab-rob/model/stack.js
@@ -9,23 +9,38 @@ class SinglyLinkedList {
 
 class Stack {
   constructor() {
-    this._data = new SinglyLinkedList(null);
+    this._data = new SinglyLinkedList(undefined);
     this._stackLength = 0;
     this._first = true;
   }
 
   push(value) {
+    if(value === undefined)
+      throw new TypeError('<value> is undefined');
     if(this._first) {
       this._data.value = value;
-      this._stackLength = 1;
       this._first = false;
     } else {
       let newStack = new SinglyLinkedList(value);
       newStack.next = this._data;
       this._data = newStack;
-      this._stackLength++;
     }
+    this._stackLength++;
     return this._stackLength;
+  }
+
+  pop() {
+    if(this._stackLength === 0)
+      return undefined;
+    let topValue = this._data.value;
+    if(this._data.next) {
+      this._data = this._data.next;
+    } else {
+      this._data.value = undefined;
+      this.first = true;
+    }
+    this._stackLength--;
+    return topValue;
   }
 }
 
