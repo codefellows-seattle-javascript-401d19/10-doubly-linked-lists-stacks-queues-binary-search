501d2c66d69ab1b4fd0487ff54a3c66f433bb6c5
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-10 17:59:59 -0800

    passing basic tests for queue

--- __test__/queue.test.js
+++ __test__/queue.test.js
@@ -23,8 +23,17 @@ describe('queue.js', () => {
 
 
     console.log(resultQueue.dequeue().value);
+    console.log(resultQueue.dequeue().previous);
     console.log(resultQueue.dequeue().next.value);
 
+    expect(resultQueue.value).toEqual(10);
+    expect(resultQueue.next.value).toEqual(20);
+    expect(resultQueue.next.next.value).toEqual(30);
+
+    expect(resultQueue.dequeue().value).toEqual(20);
+    expect(resultQueue.dequeue().previous).toEqual(null);
+    expect(resultQueue.dequeue().next.value).toEqual(30);
+    expect(resultQueue.dequeue().next.next).toEqual(null);
     // expect(resultQueue.next.value).toEqual(20);
     // expect(first.next.previous).toEqual(null);
     // expect(first.next.value).toEqual(20);
