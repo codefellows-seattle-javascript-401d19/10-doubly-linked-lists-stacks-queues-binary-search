65020f6738fc383285ad6803af79fe664dbd02c8
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-12-11 09:36:46 -0800

    added length method

--- lab-jacob/model/double-linked-list.js
+++ lab-jacob/model/double-linked-list.js
@@ -27,18 +27,39 @@ class DoubleLinkedListModule{
   remove(node){
     if(!(node instanceof DoubleLinkedListModule))
       throw new TypeError(`<node> should be an instance of DoubleLinkedList`);
-    if (!this.next){
-      this.next = node;
-      node.previous = this;
-      node.next.next = node.next;
-      node.next.next.previous.previous = node.previous; 
-    }else 
-      this.next.remove(node);
+
+    if (this === node){
+      if(this.next){
+        this.next.previous = null;
+      }else
+        return null;
+    }
+
+    if (this.next === null && this.previous === null){
+      return this === null;
+    }
+    // this.next = node;
+    // node.previous = this;
+    // node.next.next = node.next;
+    // node.next.next.previous.previous = node.previous; 
+
     
     return this;
+  }
+  
+  //LENGTH METHOD linkedListLength
+  linkedListLength(node){
+    if(!(node instanceof DoubleLinkedListModule))
+      throw new TypeError(`<node> should be an instance of DoubleLinkedList`);
+
+    let ctr = 0;  
+    while (this.next !== null) {
+      ctr++;
+      this.next = this.next.next; 
+    }
+    return ctr; 
   }
 
 }
 
-//LENGTH METHOD linkedListLength
 module.exports = DoubleLinkedListModule;
