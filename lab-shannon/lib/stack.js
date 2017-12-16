05d2266c7ecc2a0810efd71d889485ece92c5efd
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-15 21:47:30 -0800

    starting to fix stack implementation, think I successfully added push method but haven't tested

--- lab-shannon/lib/stack.js
+++ lab-shannon/lib/stack.js
@@ -1,29 +1,32 @@
 'use strict';
 
-let stackModule = module.exports = {};
-let doublyLinkList = require(`./doublyLinkList`);
-// Add and remove elements ONLY from the top of the stack
+class Stack{
+  constructor(value){
+    this.value = value,
+    this.next = null,
+    this.previous = null,
+  }
 
-let stack.createStack = () => {
-  let stack = new DoublyLinkList();
+// add something to the top of the stack
+  push(node){
+    if(!(node instanceof Stack)){
+      throw new TypeError(`To add a value to the stack you must provide an instance of Stack`);
+    }
 
-  stack.push = (element) => {
-    stack.append(element);
-  };
+    if(this.value === undefined){
+      throw new Error(`The stack does not exist. You must create one before pushing a new value`);
+    }
 
-  stack.pop = () => {
-    stack.remove();
-  };
-};
-// stack.createStack = () => {
-//   let data = [];
-//
-//   return {
-//     push: (element) => {                 // adds an element to the end of the array
-//       data.push(element);
-//     },
-//     pop: () => {                        // removes an element from the end of the array & returns that element
-//       return data.pop();
-//     }
-//   }
-// }
+    if(this.next === null){
+      this.next = node;
+      node.previous = this;
+    }else{
+      this.next.push(node);
+    }
+
+    return this;
+  }
+
+// remove something from the top of the stack
+  pop(node){}
+}
